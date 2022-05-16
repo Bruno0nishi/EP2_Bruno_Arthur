d419964f6281b17785157edb136e5862751d4d99
Arthgovis <103510935+Arthgovis@users.noreply.github.com>
2022-05-15 23:53:27 -0300

    Versão final do jogo

--- EP2/README.md
+++ EP2/README.md
@@ -1,19 +1,33 @@
 # EP2_Bruno_Arthur
 
-Descrição do projeto: O EP2 foi um exercicio de programação criado pelos professores da diciplina "Design e Software" da Instituição de Ensino e Pesquisa (Insper). O projeto consiste na implemnetação da capacidade de programação em um jogo. Esse consiste na tentativa do usuario de adivinhar um país ecolhido pelo programa da equipe. O programa possui ferramentas como caixas de dicas, distancias reais do chute em kilometros em relação ao país correto e muito mais.
+->Descrição do projeto: 
 
+O EP2 foi um exercicio de programação criado pelos professores da diciplina "Design e Software" da Instituição de Ensino e Pesquisa (Insper). O projeto consiste na implemnetação da capacidade de programação em um jogo. Esse consiste na tentativa do usuario de adivinhar um país ecolhido pelo programa da equipe. O programa possui ferramentas como caixas de dicas, distancias reais do chute em kilometros em relação ao país correto e muito mais.
 
-Descrição do processo de criação:
-1°
+->Descrição do jogo: 
+
+O jogo consiste em um jogador acertar o país sorteado pelo sistema por meio de chutes e tendo a opção de obter diversas dicas, sendo elas:  Cores da bandeira, continente, area ,população e  as letras da capital do país.
+
+->Descrição do processo de criação:
+
+1°)
 Após o lançamento da tarefa EP2 decidimos montar um esquema no papel com todas as funcionalidades do jogo para ter algo como guia e organizar encontros no Insper ou virtuais para a montagem do codigo
-2°
+2°)
 Inicialmente enviamos alguns arquivos(dev1, dev2) para testas as funcionabilidades do GitHub
-3°
+3°)
 Logo apos enviamos a base de dados, peça chave para o desenvolvimento do programa
-4°
+4°)
 Depois enviados as funções inicias do nosso jogo
-5°
+5°)
 Ao garantir que elas estavão funcionando importamos elas para o arquivo principal 
-6°
-Sem o acerto fino foi enviado um codigo bruto sem refinamento na interface do jogador
-7°
+6°)
+A partir de agora, o código principal começa (A interface inicial é criada)
+7°)
+Alguns testes com a biblioteca termcolor
+8°)
+Começamos agora a parte das dicas
+9°)
+Desta vez o avanço foi grande. Foi enviado um código completamente funcional, mas com alguns pequenos erros e com uma interfece diferente da recomendada.
+10°)
+Mudança de planos, deixamos o código mais próximo da versão que nos foi recomendada
+
--- EP2/principal.py
+++ EP2/principal.py
@@ -18,24 +18,41 @@ parar_tudo=False
 while not parar_tudo:
 
     print()
-    print(colored('|=================================|', 'cyan'))
-    print(colored('| Bem vindo(a) ao Jogo dos países |', 'cyan'))
-    print(colored('|                                 |', 'cyan'))
-    print(colored('|  Seu objetivo vai ser acertar   |', 'cyan'))
-    print(colored('| um país sortesado pelo sistema  |', 'cyan'))
-    print(colored('|=================================|', 'cyan'))
+    print(colored('       |=================================|', 'cyan'))
+    print(colored('       | Bem vindo(a) ao Jogo dos países |', 'cyan'))
+    print(colored('       |                                 |', 'cyan'))
+    print(colored('       |     Seu objetivo é acertar      |', 'cyan'))
+    print(colored('       |  um país sorteado pelo sistema  |', 'cyan'))
+    print(colored('       |                                 |', 'cyan'))
+    print(colored('       |          Boa Sorte!!            |', 'cyan'))
+    print(colored('       |=================================|', 'cyan'))
+    print()
+    print(colored('|===============================================|', 'cyan'))
+    print(colored('|                     Opções:                   |', 'cyan'))
+    print(colored('|-----------------------------------------------|', 'cyan'))
+    print(colored('| 1-Fazer um chute --> Digite o nome de um país |', 'cyan'))
+    print(colored('| 2-Obter uma dica --> Digite "dica"            |', 'cyan'))
+    print(colored('| 3-Desistir --------> Digite "desisto"         |', 'cyan'))
+    print(colored('|===============================================|', 'cyan'))
 
     Pais_escolhido= sorteia_pais(Dados_normalizados)
     lista=[]
+    dic={'cor':' Cores da bandeira: ', 'continente':'', 'area':'', 'pop':'', 'letra':' Letras da capital do país: '}
+    chaves=list(dic.keys())
+    lista_ja_escolhido=[]
     lista_de_tentativas=[]
     lista_de_dicas=[]
+    lista_dics=[]
+    restrita=[]
     lista_cores=[]
+    letra=[]
+    cor=[]
     Tentativas=20
     parar=False
 
     for i in Dados_normalizados[Pais_escolhido]['bandeira']:
         if Dados_normalizados[Pais_escolhido]['bandeira'][i]>0:
-            if i != 'outros':
+            if i != 'outras':
                 lista_cores.append(i)
 
     for i in Dados_normalizados:
@@ -48,41 +65,41 @@ while not parar_tudo:
         adiciona_em_ordem(pais, distancia, lista)
 
     while not parar:
+        parar2=False
 
-        print()
-        print()
-        print()
-        print()
-        print('|=================================|')
-        print('|                                 |')
-        print('|    1  ->  Fazer um chute        |')
-        print('|    2  ->  Obter uma dica        |')
-        print('|    3  ->  Desistir              |')
-        print('|                                 |')
-        print('|=================================|')
-        print()
-        print()
-        print()
-        Tenta_ou_Compra=input("  Pressione 1 para fazer um chute, 2 para obter uma dica e 3 para desistir  ")
-        print()
+        if len(lista_de_tentativas)>0:
+            print()
+            print(colored('Distancias:    ', 'cyan'))
+            print(colored('---------------------------------', 'cyan'))
+            print('\n'.join(lista_ja_escolhido))
+            print()
 
-        if Tenta_ou_Compra == '1':
-            parar2=False
+        if len(lista_dics)>0:
+            print(colored('Dicas:', 'cyan'))
+            print(colored('----------------------------------------------------------', 'cyan'))
+            for i in lista_dics:
+                if dic[i]==list:
+                    print(dic[i][1:len(dic[i]-1)] )
+                if dic[i]!=list:  
+                    print(dic[i])
+                print(colored('----------------------------------------------------------', 'cyan'))
 
-
-            while not parar2:
-                print()
-                pais2=input("   Digite um país:   ")
-                print()
-                pais2=pais2.lower()
+        while not parar2:
+            print()
+            pais2=input("    De seu palpite:   ")
+            print()
+            pais2=pais2.lower()
+            if pais2!='dica' and pais2!='desisto':
                 if esta_na_lista(pais2, lista)==True:
                     parar2=True
                 else:
                     print()
                     print("   O país citado infelizmente não se encontra em nosso banco de dados   ")
                     print()
+            else:
+                parar2=True
 
-
+        if pais2!='dica' and pais2!='desisto':
             for i in lista:
                 if pais2==i[0]:
                     distancia2=i[1]
@@ -91,89 +108,107 @@ while not parar_tudo:
             if len(str(distancia2))<=3:
                 x=distancia2
 
-
             if distancia2==0:
                 print()
                 print(colored("   Parabéns, você acertou!!!   ", 'magenta'))
                 print()
-                quer_parar=input('     Deseja jogar novamente?   (s/n)  ')
+                quer_parar=input('   Deseja jogar novamente?   (s/n)  ')
                 if quer_parar=='n':
                     parar_tudo=True
-                parar=True   
+                parar=True  
+            else:
+                if len(lista_ja_escolhido)==0:
+                    if distancia2>5000:
+                        lista_ja_escolhido.append(colored(f'  {pais2} ------> {x} Km', 'red'))
+                    if 2000<distancia2<=5000:
+                        lista_ja_escolhido.append(colored(f'  {pais2} ------> {x} Km', 'yellow'))
+                    if distancia2<=2000:
+                        lista_ja_escolhido.append(colored(f'  {pais2} ------> {x} Km', 'green'))        
+                    numeros.append(distancia2)
 
-            if distancia2!=0:
-                print()
-                print(x)
-                print()
-
+                else:
+                    numeros.append(distancia2)
+                    numeros.sort()
+                    if distancia2>5000:
+                        lista_ja_escolhido.insert(numeros.index(distancia2), colored(f'  {pais2} ------> {x} Km', 'red'))
+                    if 2000<distancia2<=5000:
+                        lista_ja_escolhido.insert(numeros.index(distancia2), colored(f'  {pais2} ------> {x} Km', 'yellow'))
+                    if distancia2<=2000:
+                        lista_ja_escolhido.insert(numeros.index(distancia2), colored(f'  {pais2} ------> {x} Km', 'green'))  
 
             if pais not in lista_de_tentativas:
                 Tentativas-=1
                 lista_de_tentativas.append(pais2)
 
-
             if distancia!=0:
 
                 if 15<=Tentativas<=20:
                     print()
-                    print (f"   Você tem mais {Tentativas} tentativa(s)   ")
+                    print (colored(f"   Você tem mais {Tentativas} tentativa(s)   ", 'green'))
+                    print()
+                if 5<Tentativas<15:
+                    print()
+                    print (colored(f"   Você tem mais {Tentativas} tentativa(s)   ", 'yellow'))
+                    print()
+                if 0<=Tentativas<=5:
+                    print()
+                    print (colored(f"   Você tem mais {Tentativas} tentativa(s)   ", 'red'))
                     print()
 
+        if pais2 == 'dica':
 
-        if Tenta_ou_Compra == '2':
-
             print()
             print()
             print()
-            print('|=================================|')
-            print('| Escolha uma das opções a baixo  |')
-            print('|---------------------------------|')
+            print(colored('|=================================|', 'cyan'))
+            print(colored('| Escolha uma das opções a baixo  |', 'cyan'))
+            print(colored('|---------------------------------|', 'cyan'))
 
             if Tentativas>4:
-                print('|    Cores da bandeira do país    |')
-                print('|          Digite:"cor"           |')
-                print('|        Custo = 4 tentativas     |')
+                print(colored('|    Cores da bandeira do país    |', 'cyan'))
+                print(colored('|          Digite:"cor"           |', 'cyan'))
+                print(colored('|        Custo = 4 tentativas     |', 'cyan'))
             else:
-                print('|                                 |')
-                print('|                                 |')
-                print('|                                 |')
-            print('|---------------------------------|')
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))
+            print(colored('|---------------------------------|', 'cyan'))
             if Tentativas>3:
-                print('|     Letras de sua capital       |')
-                print('|         Digite:"letra"          |')
-                print('|        Custo = 3 tentativas     |')
+                print(colored('|      Letras de sua capital      |', 'cyan'))
+                print(colored('|         Digite:"letra"          |', 'cyan'))
+                print(colored('|        Custo = 3 tentativas     |', 'cyan'))
             else:
-                print('|                                 |')
-                print('|                                 |')
-                print('|                                 |')
-            print('|---------------------------------|')
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))
+            print(colored('|---------------------------------|', 'cyan'))
             if Tentativas>6 and 'area' not in lista_de_dicas:
-                print('|              Área               |')
-                print('|          Digite:"area"          |')
-                print('|        Custo = 6 tentativas     |')
+                print(colored('|              Área               |', 'cyan'))
+                print(colored('|          Digite:"area"          |', 'cyan'))
+                print(colored('|        Custo = 6 tentativas     |', 'cyan'))
             else:
-                print('|                                 |')
-                print('|                                 |')
-                print('|                                 |')            
-            print('|---------------------------------|')
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))            
+            print(colored('|---------------------------------|', 'cyan'))
             if Tentativas>5 and 'pop' not in lista_de_dicas:
-                print('|           População             |')
-                print('|          Digite:"pop"           |')
-                print('|        Custo = 5 tentativas     |')
+                print(colored('|           População             |', 'cyan'))
+                print(colored('|          Digite:"pop"           |', 'cyan'))
+                print(colored('|        Custo = 5 tentativas     |', 'cyan'))
             else:
-                print('|                                 |')
-                print('|                                 |')
-                print('|                                 |')            
-            print('|---------------------------------|')
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))            
+            print(colored('|---------------------------------|', 'cyan'))
             if Tentativas>7 and 'continente' not in lista_de_dicas:
-                print('|           Continente            |')
-                print('|       Digite:"continente"       |')
-                print('|        Custo = 7 tentativas     |')
+                print(colored('|           Continente            |', 'cyan'))
+                print(colored('|       Digite:"continente"       |', 'cyan'))
+                print(colored('|       Custo = 7 tentativas      |', 'cyan'))
             else:
-                print('|                                 |')
-                print('|                                 |')
-                print('|                                 |')
-            print('|=================================|')
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))
+                print(colored('|                                 |', 'cyan'))
+            print(colored('|=================================|', 'cyan'))
             print()
             print()
             parar3=False
@@ -191,13 +226,138 @@ while not parar_tudo:
                 else:
                     print(' Esta dica não existe')   
 
+            if dica=='cor':
+                
+                if Tentativas <= 4:
+                    print()
+                    print(colored(' Você não possui tentativas sulficientes para comprar essa dica', 'red'))
+                    print()
 
-            if dica =='cor':
-                x=random.choice(lista_cores)     
-                print (f'Uma das cores é : {x}') 
-                lista_cores.remove(x)
-                    
-        if Tenta_ou_Compra == '3':
+                else:
+                    if len(lista_cores)>0:
+                        x=random.choice(lista_cores)
+                        print()
+                        b=dic['cor']+str(x)+', '
+                        dic['cor'] = b
+                        print()
+                        lista_cores.remove(x)
+                        if 'cor' not in lista_dics:
+                            lista_dics.append('cor')
+                        Tentativas-= 4
+                    else:
+                        print(colored(' As cores principais da bandeira do país já foram reveladas ', 'red'))
+
+
+            if dica=='letra':
+
+                if Tentativas <=  3 :
+                    print()
+                    print(colored(' Você não possui tentativas sulficientes para comprar essa dica ', 'red'))
+                    print()
+
+                else:   
+                    if len(Dados_normalizados[Pais_escolhido]["capital"])==len(restrita):
+                        print()
+                        print(' Todas as letras da já foram reveladas ')
+                        print()
+                    else:
+                        x=sorteia_letra(Dados_normalizados[Pais_escolhido]["capital"], restrita)
+                        print()
+                        b=dic['letra']+str(x)+', '
+                        dic['letra'] = b
+                        print()
+                        restrita.append(x)
+                        if 'letra' not in lista_dics:
+                            lista_dics.append('letra')
+                        Tentativas-= 3
+
+            if dica=='pop':
+
+                if 'pop' in lista_de_dicas:
+                    print()
+                    print(colored(' Esta dica já foi previamente escolhida', 'red'))
+                    print()
+
+                else:
+                    lista_de_dicas.append('pop')
+
+                    if Tentativas <= 5  :
+                        print()
+                        print(colored(' Você não possui tentativas sulficientes para comprar essa dica', 'red'))
+                        print()
+
+                    else:
+                        print()
+                        b=' O país tem {} habitantes'.format(Dados_normalizados[Pais_escolhido]['populacao'])
+                        dic['pop']=b
+                        print()
+                        lista_dics.append('pop')
+                        Tentativas-= 5
+
+            if dica=='area':
+
+                if 'area' in lista_de_dicas:
+                    print()
+                    print(colored(' Esta dica já foi previamente escolhida', 'red'))
+                    print()
+
+                else:
+                    lista_de_dicas.append('area')
+
+                    if Tentativas <=  6 :
+                        print()
+                        print(colored(' Você não possui tentativas sulficientes para comprar essa dica', 'red'))
+                        print()
+
+                    else:
+                        print()
+                        b=' O país tem uma área de {} metros quadrados'.format(Dados_normalizados[Pais_escolhido]['area'])
+                        dic['area']=b
+                        print()
+                        lista_dics.append('area')
+                        Tentativas-= 6
+
+            if dica=='continente':
+
+                if 'continente' in lista_de_dicas:
+                    print()
+                    print(colored(' Esta dica já foi previamente escolhida', 'red'))
+                    print()
+
+                else:
+                    lista_de_dicas.append('continente')
+
+                    if Tentativas <=  7 :
+                        print()
+                        print(colored(' Você não possui tentativas sulficientes para comprar essa dica', 'red'))
+                        print()
+
+                    else:
+                        print()
+                        b=' É um país da {}'.format(Dados_normalizados[Pais_escolhido]['continente'])
+                        dic['continente']=b
+                        print()
+                        lista_dics.append('continente')
+                        Tentativas-= 7
+
+            if Tentativas!=0:
+
+                if 15<=Tentativas<=20:
+                    print()
+                    print (colored(f"   Você tem mais {Tentativas} tentativa(s)   ", 'green'))
+                    print()
+
+                if 5<Tentativas<15:
+                    print()
+                    print (colored(f"   Você tem mais {Tentativas} tentativa(s)   ", 'yellow'))
+                    print()
+
+                if 0<=Tentativas<=5:
+                    print()
+                    print (colored(f"   Você tem mais {Tentativas} tentativa(s)   ", 'red'))
+                    print()
+
+        if pais2 == 'desisto':
 
             certeza=input('      Tem certeza que deseja desistir?   (s/n)   ')
 
@@ -220,9 +380,9 @@ while not parar_tudo:
         if Tentativas==0:
 
             print()
-            print("     Suas tentativas se esgotaram     ")
+            print(colored("     Suas tentativas se esgotaram     ", 'red'))
             print()
-            print(f'   O país correto era :{Pais_escolhido}     ')
+            print(colored(f'     O país correto era :{Pais_escolhido}     ', 'cyan'))
             print()
             parar=True
             quer_parar=input('     Deseja jogar novamente?   (s/n)  ')
@@ -234,5 +394,3 @@ while not parar_tudo:
                 print("        Até a proxima     ")
                 print()
                 parar_tudo=True
-
-
